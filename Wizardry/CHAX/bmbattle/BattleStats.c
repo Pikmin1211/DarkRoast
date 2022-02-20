bf90f9bb4eb092af8ffb02d0f47e2d71092041eb
Pikmin1211 <jfarr095@uottawa.ca>
2022-02-19 20:01:27 -0500

    crit + dodge formulae

--- Wizardry/CHAX/bmbattle/BattleStats.c
+++ Wizardry/CHAX/bmbattle/BattleStats.c
@@ -1,3 +1,11 @@
 void ComputeBattleUnitHitRate(struct BattleUnit* battleUnit){
 	battleUnit->battleHitRate = (battleUnit->unit.skl * 2) + battleUnit->unit.lck + GetItemHit(battleUnit->weapon) + battleUnit->wTriangleHitBonus;
 }
+
+void ComputeBattleUnitCritRate(struct BattleUnit* battleUnit){
+	battleUnit->battleCritRate = battleUnit->unit.skl + GetItemCrit(battleUnit->weapon);
+}
+
+void ComputeBattleUnitDodgeRate(struct BattleUnit* battleUnit){
+	battleUnit->battleDodgeRate = battleUnit->unit.lck*2;
+}
