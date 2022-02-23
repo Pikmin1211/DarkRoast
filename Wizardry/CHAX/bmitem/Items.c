ca5f3bbb913721105ef38cd3a727c78f19667e84
Pikmin1211 <jfarr095@uottawa.ca>
2022-02-23 16:34:14 -0500

    GetUnitItemHealAmount

--- Wizardry/CHAX/bmitem/Items.c
+++ Wizardry/CHAX/bmitem/Items.c
@@ -1,9 +1,11 @@
-int GetUnitItemHealAmount(struct Unit* unit, int item) {
+int GetUnitItemHealAmount(const struct Unit* unit, Item item) {
     int result = 0;
 
     result += GetItemMight(item);
 
     if (GetItemAttributes(item) & IA_STAFF) {
         result += GetUnitPower(unit);
+    }
 
     return result;
+}
--- Wizardry/CHAX/src.c
+++ Wizardry/CHAX/src.c
@@ -1,3 +1,4 @@
 #include "FE-CLib/include/gbafe.h"
 
 #include "bmbattle/BattleStats.c"
+#include "bmitem/Items.c"
