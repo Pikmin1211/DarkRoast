f459fda0925adfc4ca0709411d7f080d917b60ac
Pikmin1211 <jfarr095@uottawa.ca>
2022-02-23 16:28:27 -0500

    herbs

---
+++ Wizardry/CHAX/bmitem/Items.c
@@ -0,0 +1,9 @@
+int GetUnitItemHealAmount(struct Unit* unit, int item) {
+    int result = 0;
+
+    result += GetItemMight(item);
+
+    if (GetItemAttributes(item) & IA_STAFF) {
+        result += GetUnitPower(unit);
+
+    return result;
