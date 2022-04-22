844abd71f24207f7360c7a2a19da0f0c9d9d65cf
Pikmin1211 <jfarr095@uottawa.ca>
2022-04-22 14:24:35 -0400

    ComputeBattleUnitCritRate adjustment

--- Wizardry/CHAX/bmbattle/BattleStats.c
+++ Wizardry/CHAX/bmbattle/BattleStats.c
@@ -7,16 +7,21 @@ void ComputeBattleUnitAvoidRate(struct BattleUnit* battleUnit) {
 }
 
 void ComputeBattleUnitCritRate(struct BattleUnit* battleUnit){
-	battleUnit->battleCritRate = battleUnit->unit.skl + GetItemCrit(battleUnit->weapon);
+	battleUnit->battleCritRate = (battleUnit->unit.skl / 2) + GetItemCrit(battleUnit->weapon);
+
+    if (UNIT_ATTRIBUTES(&battleUnit->unit) & CA_CRITBONUS){
+        battleUnit->battleCritRate += 15;
+    }
 
     if (GetItemCrit(battleUnit->weapon) == 0xFF){
         battleUnit->battleCritRate = 0;
     }
+
 }
 
 /* see asm folder
 void ComputeBattleUnitDodgeRate(struct BattleUnit* battleUnit){
-	battleUnit->battleDodgeRate = battleUnit->unit.lck*2;
+	battleUnit->battleDodgeRate = battleUnit->unit.lck * 2;
 }
 */
 
