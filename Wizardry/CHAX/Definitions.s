3569f96024b7f9b391ce6d62b704cf97a5b61fc9
Pikmin1211 <jfarr095@uottawa.ca>
2022-04-22 20:35:37 -0400

    growth getters

--- Wizardry/CHAX/Definitions.s
+++ Wizardry/CHAX/Definitions.s
@@ -2,3 +2,7 @@
 .include "FE-CLib/reference/FE8U-20190316.s"
 
 SET_FUNC ExecKeyOrLockpick, 0x802F510 + 1
+
+SET_FUNC CanBattleUnitGainLevels, 0x802B9F4 + 1
+
+SET_FUNC CheckBattleUnitStatCaps, 0x802BF24 + 1
--- Wizardry/CHAX/bmbattle/BattleStats.c
+++ Wizardry/CHAX/bmbattle/BattleStats.c
@@ -86,3 +86,78 @@ s8 BattleGetFollowUpOrder(struct BattleUnit** outAttacker, struct BattleUnit** o
 
     return TRUE;
 }
+
+int ApplyGlobalGrowthModifiers(struct Unit* unit, int growth){
+    growth += unit->lck>>1;
+    
+    if (unit->state & US_GROWTH_BOOST){
+        growth += 5;
+    }
+
+    return growth;
+}
+
+int GetUnitHPGrowth(struct Unit* unit){
+    int growth = unit->pCharacterData->growthHP;
+    return ApplyGlobalGrowthModifiers(unit, growth);
+}
+
+int GetUnitPowGrowth(struct Unit* unit){
+    int growth = unit->pCharacterData->growthPow;
+    return ApplyGlobalGrowthModifiers(unit, growth);
+}
+
+int GetUnitSklGrowth(struct Unit* unit){
+    int growth = unit->pCharacterData->growthSkl;
+    return ApplyGlobalGrowthModifiers(unit, growth);
+}
+
+int GetUnitSpdGrowth(struct Unit* unit){
+    int growth = unit->pCharacterData->growthSpd;
+    return ApplyGlobalGrowthModifiers(unit, growth);
+}
+
+int GetUnitDefGrowth(struct Unit* unit){
+    int growth = unit->pCharacterData->growthDef;
+    return ApplyGlobalGrowthModifiers(unit, growth);
+}
+
+int GetUnitResGrowth(struct Unit* unit){
+    int growth = unit->pCharacterData->growthRes;
+    return ApplyGlobalGrowthModifiers(unit, growth);
+}
+
+int GetUnitLckGrowth(struct Unit* unit){
+    int growth = unit->pCharacterData->growthLck;
+    return ApplyGlobalGrowthModifiers(unit, growth);
+}
+
+bool CanBattleUnitGainLevels(struct BattleUnit* battleUnit);
+void CheckBattleUnitStatCaps(struct Unit* unit, struct BattleUnit* battleUnit);
+
+void CheckBattleUnitLevelUp(struct BattleUnit* battleUnit) {
+    if (CanBattleUnitGainLevels(battleUnit) && battleUnit->unit.exp >= 100) {
+        battleUnit->unit.exp -= 100;
+        battleUnit->unit.level++;
+
+        if (battleUnit->unit.level == 20) {
+            battleUnit->expGain -= battleUnit->unit.exp;
+            battleUnit->unit.exp = UNIT_EXP_DISABLED;
+        }
+
+        int REROLL_ATTEMPTS = 2;
+        for(int i = 0; i < REROLL_ATTEMPTS; i++){
+            battleUnit->changeHP  = GetStatIncrease(GetUnitHPGrowth(&battleUnit->unit));
+            battleUnit->changePow = GetStatIncrease(GetUnitPowGrowth(&battleUnit->unit));
+            battleUnit->changeSkl = GetStatIncrease(GetUnitSklGrowth(&battleUnit->unit));
+            battleUnit->changeSpd = GetStatIncrease(GetUnitSpdGrowth(&battleUnit->unit));
+            battleUnit->changeDef = GetStatIncrease(GetUnitDefGrowth(&battleUnit->unit));
+            battleUnit->changeRes = GetStatIncrease(GetUnitResGrowth(&battleUnit->unit));
+            battleUnit->changeLck = GetStatIncrease(GetUnitLckGrowth(&battleUnit->unit));
+            if (battleUnit->changeHP + battleUnit->changePow + battleUnit->changeSkl + battleUnit->changeSpd + battleUnit->changeDef + battleUnit->changeRes + battleUnit->changeLck){
+                break;
+            }
+        }
+        CheckBattleUnitStatCaps(GetUnit(battleUnit->unit.index), battleUnit);
+    }
+}
