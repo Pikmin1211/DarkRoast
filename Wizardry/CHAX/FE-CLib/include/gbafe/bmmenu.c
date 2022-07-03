f52c91f78cae3a01c5d18abc70369f15cddafa05
pandan-epic-hacking <91435101+pandan-epic-hacking@users.noreply.github.com>
2022-07-03 14:13:51 -0700

    steal changes part uno

--- Wizardry/CHAX/FE-CLib/include/gbafe/bmmenu.c
+++ Wizardry/CHAX/FE-CLib/include/gbafe/bmmenu.c
@@ -30,3 +30,37 @@ if (GetTargetListSize()==0){
 return MCA_USABLE;
 
 }
+
+// New steal function
+
+void MakeTargetListForSteal (Unit* unit);
+
+u8 StealCommandUsability(){
+	
+if (gActiveUnit -> state & US_CANTOING){
+	return MCA_NONUSABLE;}
+	
+switch(gActiveUnit -> pClassData -> number){
+case Thief:
+case Thief_F:
+case Diabolist:
+case Outlaw:
+case Grassrunner:
+case Outlaw_F:
+case Grassrunner_F:
+case Roughneck:
+case Boss:
+break;
+default:
+return MCA_NONUSABLE;
+break;
+}
+
+MakeTargetListForSteal(gActiveUnit);
+if (GetTargetListSize()==0){
+	return MCA_NONUSABLE;}
+	
+if (GetUnitItemCount(gActiveUnit)==5){
+	return MCA_GRAYED;}
+
+return MCA_USABLE;
